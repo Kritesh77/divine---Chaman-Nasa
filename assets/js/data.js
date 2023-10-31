3165cdb2cba4122b92a56797bd8e21dd3702d37c
Kritesh Kaushik <kriteshkaushik77@gmail.com>
2023-11-01 02:07:32 +0530

    init

---
+++ assets/js/data.js
@@ -0,0 +1,223 @@
+  const dataT = [
+    {
+      id: "a11",
+      category: "Shipping / Logistics packaging (Heavy cartons, wooden crates)",
+      products: [
+        {
+          images: ["../assets/img/products/a11.jpeg"],
+          title: "Master Carton Packaging boxes (Plain / Printed / Branded)",
+        },
+      ],
+    },
+    {
+      id: "a12",
+      category: "Shipping / Logistics packaging (Heavy cartons, wooden crates)",
+      images: "",
+      products: [
+        {
+          images: ["../assets/img/products/a12.png"],
+          title: "Nail-less Wooden Packaging boxes",
+        },
+      ],
+    },
+    {
+      id: "a13",
+      category: "Shipping / Logistics packaging (Heavy cartons, wooden crates)",
+      images: "",
+      products: [
+        {
+          images: ["../assets/img/products/a13.webp"],
+          title: "Honey Comb Packaging (Pallets, Boxes)",
+        },
+      ],
+    },
+    {
+      id: "b11",
+      category: "foodIndustry",
+      products: [
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title: "Vegetables/ Fruits Packaging Boxes",
+        },
+      ],
+    },
+    {
+      id: "b12",
+      category: "automotiveIndustry",
+      products: [
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title:
+            "Spare parts boxes (Head Lamps, Motors, Tail Lights, Horns, Tapes)",
+        },
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title: "Wire boxes",
+        },
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title: "Other Spare Parts",
+        },
+      ],
+    },
+    {
+      id: "b13",
+      category: "ecommerceIndustry",
+      products: [
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title:
+            "Online order fulfillment boxes (Food and vegetables online delivery)",
+        },
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title: "Ecommerce Groceries Deliveries",
+        },
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title: "Ecommerce Fashion",
+        },
+      ],
+    },
+    {
+      id: "b13",
+      category: "liquorIndustry",
+      products: [
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title: "Liquor Product Packaging boxes",
+        },
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title: "Liquor Master Corrugated boxes",
+        },
+      ],
+    },
+    {
+      id: "b14",
+      category: "cosmeticsIndustry",
+      products: [
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title:
+            "Cosmetics products (Creams, Facial Packages, Massage products boxes)",
+        },
+      ],
+    },
+    {
+      id: "b15",
+      category: "electricalAppliances",
+      products: [
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title: "Electrical goods and gadgetries",
+        },
+      ],
+    },
+    {
+      id: "b16",
+      category: "whiteGoods",
+      products: [
+        {
+          images: "images/products/canvas-shopping-bags-500x500.webp",
+          title: "White goods outer covering carton boxes",
+        },
+      ],
+    },
+  ];
+
+  const data = [
+    {
+      id: "a11",
+      category: "Shipping / Logistics packaging (Heavy cartons, wooden crates)",
+      images: ["../assets/img/products/a11.jpeg"],
+      title: "Master Carton Packaging boxes (Plain / Printed / Branded)",
+    },
+    {
+      id: "a12",
+      category: "Shipping / Logistics packaging (Heavy cartons, wooden crates)",
+      images: ["../assets/img/products/a12.png"],
+      title: "Nail-less Wooden Packaging boxes",
+    },
+    {
+      id: "a13",
+      category: "Shipping / Logistics packaging (Heavy cartons, wooden crates)",
+      images: ["../assets/img/products/a13.webp"],
+      title: "Honey Comb Packaging (Pallets, Boxes)",
+    },
+    {
+      id: "b11",
+      category: "foodIndustry",
+      images: ["../assets/img/products/b11.jpg"],
+      title: "Vegetables/ Fruits Packaging Boxes",
+    },
+    {
+      id: "b12",
+      category: "automotiveIndustry",
+      images: ["../assets/img/products/b12.jpg"],
+      title: "Spare parts boxes (Head Lamps, Motors, Tail Lights, Horns, Tapes)",
+    },
+    {
+      id: "b13",
+      category: "automotiveIndustry",
+      images: ["../assets/img/products/b13.webp"],
+      title: "Wire boxes",
+    },
+    {
+      id: "b14",
+      category: "automotiveIndustry",
+      images: ["../assets/img/products/b14.jpeg"],
+      title: "Other Spare Parts",
+    },
+    {
+      id: "b15",
+      category: "ecommerceIndustry",
+      images: ["../assets/img/products/b15.jpg"],
+      title:
+        "Online order fulfillment boxes (Food and vegetables online delivery)",
+    },
+    {
+      id: "b16",
+      category: "ecommerceIndustry",
+      images: ["../assets/img/products/b16.jpeg"],
+      title: "Ecommerce Groceries Deliveries",
+    },
+    {
+      id: "b17",
+      category: "ecommerceIndustry",
+      images: ["../assets/img/products/b17.jpg"],
+      title: "Ecommerce Fashion",
+    },
+    {
+      id: "b18",
+      category: "liquorIndustry",
+      images: ["../assets/img/products/b18.webp"],
+      title: "Liquor Product Packaging boxes",
+    },
+    {
+      id: "b19",
+      category: "liquorIndustry",
+      images: ["../assets/img/products/b19.webp"],
+      title: "Liquor Master Corrugated boxes",
+    },
+    {
+      id: "b20",
+      category: "cosmeticsIndustry",
+      images: ["../assets/img/products/b20.jpg"],
+      title:
+        "Cosmetics products (Creams, Facial Packages, Massage products boxes)",
+    },
+    {
+      id: "b21",
+      category: "electricalAppliances",
+      images: ["../assets/img/products/b21.jpg"],
+      title: "Electrical goods and gadgetries",
+    },
+    {
+      id: "b22",
+      category: "whiteGoods",
+      images: ["../assets/img/products/b22.jpg"],
+      title: "White goods outer covering carton boxes",
+    },
+  ];
+  
